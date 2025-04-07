7c7a42000487fd429b5cd1dea39c2a00efb698c2
radyelkasas <radyelkasas100@gmail.com>
2025-04-07 09:06:37 +0200

    fix: update protected route matcher for webhook middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/api/webhooks/*"]);
+const isProtectedRoute = createRouteMatcher(["/api/webhooks/clerk"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
 });
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
